e97515bbf2c08eccbb687bb55113ec2be68a0fda
Kent Kim <kent@ipl.com>
2026-07-17 16:38:40 +0000

    PWA + 8 languages + 185 countries + leaderboard + mypage

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'ipl-v1';
+const ASSETS = ['/', '/index.html', '/logo.webp', '/manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
